384d898f843947e7c6c9e83c35f52bda512a2f9b
Vincent Croos <home@Vincents-MacBook-Pro.local>
2024-05-08 07:07:43 -0500

    Add comments

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ let welcomeEl = document.getElementById("welcome-el");
 welcomeEl.innerText = greeting + myName + String.fromCodePoint(128512); //code for emoji that doesn't display on Chrome
 
 let count = 0;
+
+//read DOM elements
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
 
+//update count
 function increment() {
   count++;
   countEl.innerText = count;
 }
 
+//update save count and reset count to 0
 function save() {
   saveEl.textContent.length === 17
     ? (saveEl.textContent += " " + count)
